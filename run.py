f50ccea122b1bcc1dce84870ee019fd3d67aa461
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-08-05 08:40:35 +0200

    début page login

--- run.py
+++ run.py
@@ -1,20 +1,15 @@
-# from flask import Flask, render_template
-# from os import listdir
-# from os.path import isfile, join
-# app = Flask(__name__)
-
-
-# @app.route("/")
-# def home():
-#     a = ""
-#     # for i in listdir("./app/templates"):
-#     #     a = a + i + "<br>"
-#     return render_template("./app/templates/index.html")
 from flask import Flask, render_template
 app = Flask(__name__)
- 
+
+
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
-   app.run()
+    app.run()
