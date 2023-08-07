d50cc342a29523fb3044174363067d9f09eb3b5a
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-08-07 18:50:50 +0200

    login/register functional

--- run.py
+++ run.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
 import db
+import bcrypt
+import werkzeug.security
+
 app = Flask(__name__)
-dBCursor = ""
+
+db.connexionDB()
 
 
 @app.route('/')
 def home():
-    global dBCursor
-    dBCursor = db.connexionDB
+    db.connexionDB()
     return render_template('login.html')
 
 
@@ -20,6 +23,27 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html')
+
+
+@app.route('/tryLog', methods=['POST'])
+def tryLog():
+    name = request.form['name']
+    pwd = request.form['password']
+    test = db.verify(name, pwd)
+    if (test):
+        print("true")
+    else:
+        print("false")
+    return render_template('register.html')
+
+
+@app.route('/tryReg', methods=['POST'])
+def tryReg():
+    name = request.form['name']
+    pwd = request.form['password']
+    hashPassword = werkzeug.security.generate_password_hash(pwd)
+    db.addData(name, hashPassword)
+    return render_template('login.html')
 
 
 if __name__ == '__main__':
