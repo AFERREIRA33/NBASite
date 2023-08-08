3eb8eaa3ee406f0bc2c2894f90d3ac9017613a09
AFERREIRA33 <92852107+AFERREIRA33@users.noreply.github.com>
2023-08-08 22:11:33 +0200

    page with a specific player + debut pagination on all player

--- player.py
+++ player.py
@@ -2,15 +2,32 @@ import urllib.request
 import json
 
 
-def getAllPlayer():
+def getAllPlayer(pageNum):
     result = []
-    url = "https://www.balldontlie.io/api/v1/players?per_page=100&page=0"
+    url = "https://www.balldontlie.io/api/v1/players?per_page=100&page=" + pageNum
     response = urllib.request.urlopen(url)
     data = response.read()
     dict = json.loads(data)
-    for i in range(100):
+    num = 0
+    if dict['meta']['total_count'] - (int(pageNum)*100) > 100:
+        num = 100
+    else:
+        num = dict['meta']['total_count'] - (int(pageNum)*100) + 100
+    print("////////////////////////////////////////////////////" +
+          str(num) + " / " + pageNum + " / " + str(dict['meta']['total_count']))
+    for i in range(num):
         firstName = dict['data'][i]['first_name']
         lastName = dict['data'][i]['last_name']
-        result.append({"firstName": firstName, "lastName": lastName})
-
+        idPlayer = dict['data'][i]['id']
+        result.append(
+            {"firstName": firstName, "lastName": lastName, "idPlayer": idPlayer})
     return result
+
+
+def getAPlayer(idPlayer):
+    result = []
+    url = "https://www.balldontlie.io/api/v1/players/" + idPlayer
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    dict = json.loads(data)
+    return dict
--- run.py
+++ run.py
@@ -45,10 +45,23 @@ def tryReg():
     return render_template('login.html')
 
 
-@app.route('/joueurs')
+@app.route('/joueurs', methods=['GET'])
 def allPlayerPage():
-    allPlayerList = player.getAllPlayer()
+    page = request.args.get('pageNum')
+    allPlayerList = player.getAllPlayer(page)
     return render_template('player.html', allPlayer=allPlayerList)
+
+
+@app.route('/joueur', methods=['POST'])
+def playerPage():
+    idPlayer = request.form['id']
+    playerInfo = player.getAPlayer(idPlayer)
+    return render_template('aPlayer.html', playerInfo=playerInfo)
+
+
+@app.route('/equipe', methods=['POST'])
+def equipePage():
+    return "toto"
 
 
 if __name__ == '__main__':
